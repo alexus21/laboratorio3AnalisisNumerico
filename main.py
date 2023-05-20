5f6984255d8a6be10c1ee5c379df03fb415dfbc1
alexus21 <US21003@ues.edu.sv>
2023-05-20 15:30:11 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Ulloa Serpas, Hugo Alexander
+# US21003
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Ejercicio 1
+
+
+class UBarranquilla:
+    def __init__(self, eNumber, name, department, jobHeld, academicDegree, nationality, salary):
+        self.employeeNumber = eNumber
+        self.name = name
+        self.department = department
+        self.jobHeld = jobHeld
+        self.academicDegree = academicDegree
+        self.nationality = nationality
+        self.salary = salary
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
