b028c06c586f838d5d3dd73a632632c148d90177
alexus21 <US21003@ues.edu.sv>
2023-05-20 16:57:22 -0600

    Agregado metodo de filtrado de datos del Ej. 1

--- main.py
+++ main.py
@@ -5,22 +5,61 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 # Ejercicio 1
 
-
 class UBarranquilla:
-    def __init__(self, eNumber, name, department, jobHeld, academicDegree, nationality, salary):
-        self.employeeNumber = eNumber
-        self.name = name
-        self.department = department
-        self.jobHeld = jobHeld
-        self.academicDegree = academicDegree
-        self.nationality = nationality
-        self.salary = salary
+    def __init__(self, data):
+        # Collection de datos
+        self.df = pd.DataFrame(data)
+
+    def findMaxSalaryInfo(self):
+        # Obtener el indice de la fila que contiene el mayor salario registrado
+        index = self.df["Salary"].idxmax()
+
+        # Mostrar valores
+        print("Resultado obtenidos: \n", self.df.loc[index])
+
+    def findTotalStrangersAmount(self):
+        pass
+
+def firstExercise():
+    dataDict = {"ID": [],
+                "Name": [],
+                "Departament": [],
+                "Position": [],
+                "Grade": [],
+                "Nationality": [],
+                "Salary": []}
+
+    continueOp = 1
+
+    while continueOp == 1:
+        employeeNumber = int(input("Numero del empleado: "))
+        fullName = input("Nombre y apellido: ")
+        department = input("Departamento: ")
+        jobHeld = input("Posición: ")
+        academicDegree = input("Grado académico: ")
+        nationality = input("Nacionalidad: ")
+        salary = float(input("Salario: "))
+
+        dataDict["ID"].append(employeeNumber)
+        dataDict["Name"].append(fullName)
+        dataDict["Departament"].append(department)
+        dataDict["Position"].append(jobHeld)
+        dataDict["Grade"].append(academicDegree)
+        dataDict["Nationality"].append(nationality)
+        dataDict["Salary"].append(salary)
+
+        continueOp = int(input("Para continuar, presione '1'; para salir, cualquier numero \n> "))
+
+    ub = UBarranquilla(dataDict)
+    ub.findMaxSalaryInfo()
 
 
 def main():
-    pass
+    firstExercise()
+
 
 
 if __name__ == "__main__":
